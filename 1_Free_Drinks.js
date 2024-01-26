47cdc94cae7d97fc285037761ca1d622ffaa021a
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-01-26 23:39:25 +0600

    1 no problem is solved

---
+++ 1_Free_Drinks.js
@@ -0,0 +1,18 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let Burger = 500;
+let Coke = 30;
+
+if (Burger > 500) {
+    console.log('Free Coke');
+}
+
+else {
+    console.log('Coke:',Coke);
+}
