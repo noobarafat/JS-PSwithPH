41df39db1128cc4ad7ad804c50c75ead5a180c69
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-01-30 14:56:22 +0600

    problem 9 solved

--- 8_commitment.js
+++ 8_commitment.js
@@ -11,3 +11,5 @@ while (target <= 60) {
     console.log('I will invest at least 6 hrs every single day for next 60 days!');
     target++;
 }
+
+
---
+++ 9_odd _even.js
@@ -0,0 +1,36 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+let oddNum = 60;
+while (oddNum <= 100) {
+    if (oddNum % 2 != 0) {
+        console.log('Odd Numbers:', oddNum);
+    }
+    oddNum++;
+
+}
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+let evenNum = 78;
+while (evenNum <= 98) {
+    if (evenNum % 2 === 0) {
+        console.log('Even Numbers:', evenNum);
+    }
+    evenNum++;
+
+}
+
+
+/*programming hero*/
