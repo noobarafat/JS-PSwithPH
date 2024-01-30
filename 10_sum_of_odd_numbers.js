dcc142499520578600123b1cf25a5a962b48d818
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-01-30 15:11:45 +0600

    problem 10 is solved

--- 10_sum_of_odd_numbers.js
+++ 10_sum_of_odd_numbers.js
@@ -1,18 +1,32 @@
 
+// Subtask-1:
 
-/***
+// Display sum of all the odd numbers from 81 to 131.
 
-Subtask-1:
+oddNum = 81;
+oddSum = 0;
+while (oddNum <= 131) {
 
-Display sum of all the odd numbers from 81 to 131.
+    if (oddNum % 2 != 0) {
+        oddSum = oddNum + oddSum;
+        console.log('Sum of odd numbers', oddSum);
+    }
+    oddNum++;
+}
 
- */
-/***
+// Subtask-2:
 
-Subtask-2:
+// Display sum of all the even numbers from 206 to 311.
 
-Display sum of all the even numbers from 206 to 311.
 
- */
+evenNum = 206;
+evenSum = 0;
+while (evenNum <= 311) {
 
-/*programming hero*/
+    if (evenNum % 2 === 0) {
+        evenSum = evenNum + evenSum;
+        console.log('Sum of even numbers', evenSum);
+
+    }
+    evenNum++;
+}
