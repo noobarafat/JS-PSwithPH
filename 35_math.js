d731297ad42389f2fee8b13bf42bf57ab70ae215
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-04 00:36:21 +0600

    problem 35 is solved

---
+++ 35_math.js
@@ -0,0 +1,21 @@
+// const math = {
+//     min : function min(num1, num2) {
+//         return num2;
+//     }
+// }
+
+// console.log(math.min)
+
+
+const min = Math.min(45, 5, 67, 35 , 5)
+console.log(min);
+
+const max = Math.max(45, 454, 23542, 35, 2264565, 42, 124,4);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(5-10)) //gap calculate, no matter abot plus or minus
+console.log(Math.round(2.54))
+console.log(Math.round(2.14))
+console.log(Math.floor(2.999999999)) // remove the fraction part
+console.log(Math.ceil(2.01)) // ceiling koreeee
+console.log(Math.random()*10)
