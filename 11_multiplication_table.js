a02722e90af54f83a250a0058a2e7a5ecf3dec4c
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-05 21:28:16 +0600

    Problem 11 is solved

--- 11_multiplication_table.js
+++ 11_multiplication_table.js
@@ -6,3 +6,18 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
 
 
 /*programming hero*/
+
+console.log("Enter Multiplication Number: 5");
+console.log("Enter Range: 10")
+
+for (i = 1; i <= 10; i++) {
+    let result = 5 * i;
+    console.log(`5 * ${i} = ${result}`);
+}
+
+let i = 1;
+while (i <= 10) {
+    const result = i * 5;
+    console.log(`5 * ${i} = ${result}`);
+    i++;
+}
