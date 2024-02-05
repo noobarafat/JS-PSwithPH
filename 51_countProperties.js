8d46770b4d5cbff6c1e4b7ef2177b570847d18e6
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-05 23:57:19 +0600

    problem 51 is solved

---
+++ 51_countProperties.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const keysArray = Object.keys(student);
+const count = keysArray.length;
+console.log('Total Length:', count)
