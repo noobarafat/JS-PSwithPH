b796e266378ce3997d804a613e4a61f807849c96
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-05 22:29:55 +0600

    problem 15 is solved

--- 15_square_break.js
+++ 15_square_break.js
@@ -1,3 +1,10 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+for(let i = 4; i<=100; i++){
+    if(Math.sqrt(i)%1===0){
+        console.log("Encountered the first square number:", i);
+        break;
+    }
+}
