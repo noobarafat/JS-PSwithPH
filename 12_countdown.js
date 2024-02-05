864a57be2e4764eb5114d50eae00f978f6c0d24c
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-02-05 21:56:28 +0600

    problem 12 is solved

--- 12_countdown.js
+++ 12_countdown.js
@@ -6,3 +6,9 @@ Implement a countdown timer that counts down from 21 to 15.
 
 
 /*programming hero*/
+
+for (let i = 21; i >= 15; i--) {
+    console.log(i)
+}
+
+console.log("Countdown Completed");
